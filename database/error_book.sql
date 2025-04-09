cc693849bfeb14287e8a7bec13aac93452e977c5
JXDS18FUJT <347613781@qq.com>
2025-04-09 17:59:23 +0800

    新增手风琴面板

--- database/error_book.sql
+++ database/error_book.sql
@@ -16,6 +16,21 @@
 CREATE DATABASE IF NOT EXISTS `error_book` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `error_book`;
 
+-- 导出  表 error_book.file_tags 结构
+CREATE TABLE IF NOT EXISTS `file_tags` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` char(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `desc` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  error_book.file_tags 的数据：0 rows
+DELETE FROM `file_tags`;
+/*!40000 ALTER TABLE `file_tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `file_tags` ENABLE KEYS */;
+
 -- 导出  表 error_book.file_uploads 结构
 CREATE TABLE IF NOT EXISTS `file_uploads` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -24,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `file_uploads` (
   `file_url` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `file_size` bigint(20) NOT NULL,
   `file_type` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `storage_type` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT 'local',
+  `tag_id` int(11) DEFAULT NULL,
   `status` tinyint(4) DEFAULT '1',
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -47,13 +62,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `status` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 正在导出表  error_book.users 的数据：1 rows
 DELETE FROM `users`;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `username`, `password_hash`, `created_at`, `updated_at`, `status`) VALUES
-	(1, 'admin', '123456', '2025-04-01 17:21:44', '2025-04-01 17:21:44', 1);
+	(1, 'admin', '123456', '2025-04-01 17:21:44', '2025-04-01 17:21:44', 1),
+	(2, 'vzjtu609', 'yujun1006', '2025-04-09 16:18:41', '2025-04-09 16:19:13', 1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
